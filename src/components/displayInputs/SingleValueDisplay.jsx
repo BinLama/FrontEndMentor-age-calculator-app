05552da4a3ce84cc9c6803e87c59f165e9b055f2
BinLama <binlama@yahoo.com>
2023-08-22 22:47:28 -0700

    add: timer bug fix

--- src/components/displayInputs/SingleValueDisplay.jsx
+++ src/components/displayInputs/SingleValueDisplay.jsx
@@ -4,19 +4,20 @@ const SingleValueDisplay = (props) => {
     const { label, value } = props;
 
     const [age, setAge] = useState("0");
-
+    console.log(value);
     useEffect(() => {
         let start = 0;
         if (value === "--") return setAge("--");
 
         const end = parseInt(value);
 
-        if (start === end) return;
+        if (start === end) return setAge("0");
 
         // find duration per increment
         let totalMilSecDur = 2;
         let incrementTime = (totalMilSecDur / end) * 1000;
 
+        console.log("got to interval", end);
         let timer = setInterval(() => {
             start += 1;
             setAge(String(start));
